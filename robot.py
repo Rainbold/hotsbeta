4bc52dc886947df02bd843ac7b9bfd5994c1bd93
RainbowLyte <rainbowlyte42@gmail.com>
2015-01-26 23:50:49 +0100

    robot hots

--- robot.py
+++ robot.py
@@ -1,22 +1,23 @@
 import requests
 
+login = ''
 
-def fct(id):
+def fct(id, login):
 	print '-------------------'
 	print id
 	cookies = dict(JSESSIONID='', __utma='', __utmz='', __atuvc='', PHPSESSID='')
 	payload = {'participantSweepstake.channel':'facebook', 'participant.firstName':'charlie','participant.lastName':'charliie',
 	'participant.address1': '', 'participant.city': '', 'participant.postCode':'', 'participant.facebookId':'',
-	'participant.email':'charlie'+str(id)+'@yopmail.com', 'participant.mobilePhone':'', 'participantAnswers[0].questionAnswer.id':'263363','ivs':''}
+	'participant.email':login+str(id)+'@yopmail.com', 'participant.mobilePhone':'', 'participantAnswers[0].questionAnswer.id':'263363','ivs':''}
 
 	r = requests.get("http://production.digibonus.com/digibonus/registration/28716", params=payload, cookies=cookies)
 	print r.text
 
 	payload = {'parentSweepstakeUrl':'','facebookIds':'',
-	'friends[0].name':'charlie','friends[0].email':'charlie1'+str(id)+'@yopmail.com',
-	'friends[1].name':'charlie', 'friends[1].email':'charlie2'+str(id)+'@yopmail.com',
-	'friends[2].name':'charlie', 'friends[2].email':'charlie3'+str(id)+'@yopmail.com',
-	'friends[3].name':'charlie', 'friends[3].email':'charlie4'+str(id)+'@yopmail.com',
+	'friends[0].name':'friend','friends[0].email':'friend1'+str(id)+'@yopmail.com',
+	'friends[1].name':'friend', 'friends[1].email':'friend2'+str(id)+'@yopmail.com',
+	'friends[2].name':'friend', 'friends[2].email':'friend3'+str(id)+'@yopmail.com',
+	'friends[3].name':'friend', 'friends[3].email':'friend4'+str(id)+'@yopmail.com',
 	'friends_count':''}
 
 
@@ -28,8 +29,8 @@ def fct(id):
 		print r.text
 		return -1
 
-for i in range(100,3000):
-	n = fct(i)
+for i in range(3000):
+	n = fct(i, login)
 	if n != -1:
 		print "/!\ FINI "+str(n)
 		break
